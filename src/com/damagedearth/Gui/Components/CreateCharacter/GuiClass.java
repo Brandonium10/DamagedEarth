9c2c3ee644253e0c3880b1458da07815c899d6df
wowlover687 <andrewmacu@me.com>
2013-10-05 15:44:30 -0700

    Added license notice and team information to every source file

--- src/com/damagedearth/Gui/Components/CreateCharacter/GuiClass.java
+++ src/com/damagedearth/Gui/Components/CreateCharacter/GuiClass.java
@@ -1,13 +1,24 @@
+/*******************************************************************************
+ * This is a free program developed and managed by The Predators Development Team. This
+ * source code should have a copy of the GNU General Public License. If it does not,
+ * please see <http://www.gnu.org/licenses/>. The Predators Development Team forums is
+ * located at <http://thepredators.endofinternet.net/index.php>.
+ ******************************************************************************/
+
+/*******************************************************************************
+ * This is a free program developed and managed by The Predators Development Team. This
+ * source code should have a copy of the GNU General Public License. If it does not,
+ * please see <http://www.gnu.org/licenses/>. The Predators Development Team forums is
+ * located at <http://thepredators.endofinternet.net/index.php>.
+ ******************************************************************************/
+
 package com.damagedearth.Gui.Components.CreateCharacter;
 
 import com.damagedearth.Entities.EntityPlayer;
 import com.damagedearth.Entities.EnumPlayerClass;
 import com.damagedearth.Utilities.Tesselator;
-import com.damagedearth.Utilities.TextureLoader;
 
 import java.awt.image.BufferedImage;
-
-import static org.lwjgl.opengl.GL11.*;
 
 public class GuiClass
 {
--- src/com/damagedearth/Utilities/TextureLoader.java
+++ src/com/damagedearth/Utilities/TextureLoader.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * This is a free program developed and managed by The Predators Development Team. This
+ * source code should have a copy of the GNU General Public License. If it does not,
+ * please see <http://www.gnu.org/licenses/>. The Predators Development Team forums is
+ * located at <http://thepredators.endofinternet.net/index.php>.
+ ******************************************************************************/
+
+/*******************************************************************************
+ * This is a free program developed and managed by The Predators Development Team. This
+ * source code should have a copy of the GNU General Public License. If it does not,
+ * please see <http://www.gnu.org/licenses/>. The Predators Development Team forums is
+ * located at <http://thepredators.endofinternet.net/index.php>.
+ ******************************************************************************/
+
 package com.damagedearth.Utilities;
 
 import org.lwjgl.BufferUtils;
